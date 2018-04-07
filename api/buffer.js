6258f1b335ce0f4bfadaa5d9107fe57922f77585
hakimix <star1994@hotmail.dk>
2018-04-07 17:28:33 +0200

    edit

--- api/buffer.js
+++ api/buffer.js
@@ -10,49 +10,49 @@ var graph = require('./graph');
 // Fetch data from api
 
 function cacheArticle() {
-    var scheduleAPIConsume = schedule.scheduleJob("*/1 * * * *", function() {
+    var scheduleAPIConsume = schedule.scheduleJob("*/30 * * * *", function() {
         console.log(' --- API Consume Articles --- ');
         consume.getArticles();
     });
 }
 
 function cacheFaktatjek() {
-    var scheduleAPIConsume = schedule.scheduleJob("*/1 * * * *", function() {
+    var scheduleAPIConsume = schedule.scheduleJob("*/30 * * * *", function() {
         console.log(' --- API Consume Faktatjek --- ');
         consume.getFaktatjek();
     });
 }
 
 function cacheViralspiralen() {
-    var scheduleAPIConsume = schedule.scheduleJob("*/1 * * * *", function() {
+    var scheduleAPIConsume = schedule.scheduleJob("*/30 * * * *", function() {
         console.log(' --- API Consume Viralspiralen --- ');
         consume.getViralspiralen();
     });
 }
 
 function cacheVaerdAtVide() {
-    var scheduleAPIConsume = schedule.scheduleJob("*/1 * * * *", function() {
+    var scheduleAPIConsume = schedule.scheduleJob("*/30 * * * *", function() {
         console.log(' --- API Consume Værd at vide --- ');
         consume.getVaerdAtVide();
     });
 }
 
 function cacheColumns() {
-    var scheduleAPIConsume = schedule.scheduleJob("*/1 * * * *", function() {
+    var scheduleAPIConsume = schedule.scheduleJob("*/30 * * * *", function() {
         console.log(' --- API Consume Columns --- ');
         consume.getColumns();
     });
 }
 
 function cachePerspektiv() {
-    var scheduleAPIConsume = schedule.scheduleJob("*/1 * * * *", function() {
+    var scheduleAPIConsume = schedule.scheduleJob("*/30 * * * *", function() {
         console.log(' --- API Consume Perspektiv --- ');
         consume.getPerspektiv();
     });
 }
 
 function cacheFBVideos() {
-    var scheduleAPIConsume = schedule.scheduleJob("*/1 * * * *", function() {
+    var scheduleAPIConsume = schedule.scheduleJob("*/30 * * * *", function() {
         console.log(' --- API Consume FB Videos --- ');
         graph.getFBVideos();
     });
